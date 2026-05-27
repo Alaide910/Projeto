f9f70292891110e2d45ad531f2e0fe5fa2664767
Alaide Gomes da Silva <alaide2067216@aluno.ba.senac.br>
2026-05-27 16:57:01 -0300

    Atualizão js

--- assets/js/js.scripts.js
+++ assets/js/js.scripts.js
@@ -1,1 +1,67 @@
 alert("Bem Vindo!");
+
+//let idade = 20;
+//idade = 21;
+//console.log(idade)
+
+//let estado;
+//if (true) {
+    //estado = BA;
+//}
+
+//const cidade = "Fortaleza";
+
+//const nome = "Ana";
+//nome = "Maria";
+
+
+//if (true) {
+   // var nome = "João";
+ //}
+
+ //console.log(nome);
+
+ //if (true) {
+    //let nome = "João";
+    //console.log(nome);
+
+   // }
+
+ //{
+   // let teste = 10;
+// }
+
+ //var nome = "Ana";
+ //var nome = "Maria";
+ //nome  = "Paulo";
+
+ //console.log(nome);
+ //var nome = "João";
+
+ //var nome; // Hoisted!
+
+//console.log(nome);
+ //let nome; = "João";
+
+ let nome = prompt("Digite seu nome");
+ console.log(nome);
+
+ let soma = prompt("Digite sair");
+ console.log(resposta);
+
+ let numerol = 10;
+ let numero2 = 5;
+
+ // Operações e saidas no console
+
+ console.log(numerol + numero2); //15
+ console.log(numerol - numero2); //5
+ console.log(numerol * numero2); //50
+ console.log(numerol / numero2); //2
+
+
+ let nome ="João";
+ console.log("Ola " + nome);
+
+ let nome = "Maria";
+ console.log('Ola $(nome)');
